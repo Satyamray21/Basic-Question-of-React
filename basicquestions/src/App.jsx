eec693c3136519aeacec8942f2276c187b0df84d
Satyamray21 <satyamray43@gmail.com>
2024-07-12 18:55:26 +0530

    Code for profile

---
+++ basicquestions/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Profile from './compontent/Profile/Profile';
+function App() {
+  return (
+   <>
+   <Profile name='Satyam'
+    age='23' 
+    bio='Software Engineer'/>  
+   </>
+   
+  );
+}
+
+export default App;
---
+++ basicquestions/src/compontent/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Profile({name,age,bio}) {
+  return (
+    <div>
+      Name:{name}
+      Age:{age}
+      Bio:{bio}
+    </div>
+  )
+}
